3819378d0bddea76a51d1399a91fa984016790a4
Imran <polboy777@gmail.com>
2019-09-25 18:37:24 +0600

    shuffle the answers

--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -3,9 +3,10 @@
     <h2 v-html="question.question"></h2>
     <b-list-group>
       <b-list-group-item
-        v-for="(answer, index) in answers"
+        v-for="(answer, index) in shuffledAnswers"
         :key="index"
         v-html="answer"
+        @click="selectedAnswer(index)"
       ></b-list-group-item>
     </b-list-group>
     <button @click="nextQuestionMethod">Next</button>
@@ -13,18 +14,55 @@
 </template>
 
 <script>
+import _ from 'lodash';
+
 export default {
   name: 'QuestionBox',
   props: {
     question: Object,
     nextQuestionMethod: Function
   },
+  data() {
+    return {
+      selectedIndex: null,
+      shuffledAnswers: []
+    };
+  },
   computed: {
     answers() {
       return [...this.question.incorrect_answers, this.question.correct_answer];
+    }
+  },
+  methods: {
+    selectedAnswer(index) {
+      this.selectedIndex = index;
+    },
+    shuffleAnswers() {
+      this.shuffledAnswers = _.shuffle(this.answers);
+    }
+  },
+  watch: {
+    question: {
+      immediate: true,
+      handler() {
+        this.selectedIndex = null;
+        this.shuffleAnswers();
+      }
     }
   }
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.selected {
+  background-color: lightblue;
+}
+
+.correct {
+  background-color: lightblue;
+}
+
+.incorrect {
+  background-color: lightblue;
+}
+</style>
