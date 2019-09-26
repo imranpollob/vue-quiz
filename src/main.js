ad0f635cae281df3db36802de60170a63c9866a9
Imran <polboy777@gmail.com>
2019-09-26 15:41:22 +0600

    bootstrap-vue removed

--- src/main.js
+++ src/main.js
@@ -1,12 +1,6 @@
 import Vue from 'vue';
-import BootstrapVue from 'bootstrap-vue';
-
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap-vue/dist/bootstrap-vue.css';
-
 import App from './App.vue';
 
-Vue.use(BootstrapVue);
 Vue.config.productionTip = false;
 
 new Vue({
